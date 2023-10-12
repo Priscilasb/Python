1469cffa7b087ad77cd19ed66aa9d2df5105d43f
Priscila dos Santos Benedito <90283421+Priscilasb@users.noreply.github.com>
2023-10-12 01:12:24 -0300

    Delete exe001.py

--- exe001.py
+++
@@ -1,9 +0,0 @@
-#Olá Mundo
-
-print('Hi, world!')
-msg = 'Hi, world'
-print(msg)
-
-#O python da algumas sugestões para melhoria
-#1 - Assim que terminar a linha de codigo, é bom que voce de ENTER e crie uma linha nova sem nada
-#2 - Ele da sugestões de identação, para que fique tudo bonitinho
