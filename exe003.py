895fe93738ace2ce49904dd95e16b48f71100051
Priscila dos Santos Benedito <90283421+Priscilasb@users.noreply.github.com>
2023-10-12 01:13:00 -0300

    Delete exe003.py

--- exe003.py
+++
@@ -1,9 +0,0 @@
-#somando dois valores
-
-valor1 = (int(input("Digite um valor:")))
-valor2 = (int(input("Digite outro valor:")))
-soma = valor1+valor2
-print(f"A soma entre {valor1} e {valor2} é {soma}")
-print("A soma entre {} e {} é {}".format(valor1,valor2,soma))
-
-
